fcd056bbe54c2e2bc6f1ef6efe47e38c2a36f0fb
FuzzyImages <mursa.artdragons@gmail.com>
2025-01-23 17:00:35 -0800

    "Added player object and draw to screen"

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from constants import *
+from player import *
 import pygame
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
     clock = pygame.time.Clock()
     dt = 0
 
@@ -13,8 +15,9 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        
         dt = clock.tick(60) /1000
 
 if __name__ == "__main__":
