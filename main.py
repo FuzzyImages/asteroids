510a5b1771170a4ed33eadcc02e4d5bcd8c6ad30
FuzzyImages <mursa.artdragons@gmail.com>
2025-01-26 23:05:24 -0800

    "Added firing mechanic"

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from constants import *
 from player import Player
+from shot import Shot
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 
@@ -14,12 +15,15 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
-    Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
 
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
 
 
