bb939a52d744fd54d5b80b047c3fe2c4735b3792
FuzzyImages <mursa.artdragons@gmail.com>
2025-01-26 20:42:40 -0800

    "Minor organization fix. Tutorial guided clean up"

--- asteroid.py
+++ asteroid.py
@@ -1,12 +1,9 @@
-from circleshape import *
-from constants import *
+import pygame
+from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
