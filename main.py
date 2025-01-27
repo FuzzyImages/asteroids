c5adb78bb232864e05b6be9d3f87cb704db98f17
FuzzyImages <mursa.artdragons@gmail.com>
2025-01-26 21:34:22 -0800

    "Added Game Over state"

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -30,6 +31,10 @@ def main():
         screen.fill("black")
         for obj in updatable:
             obj.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game Over")
+                sys.exit()
         for obj in drawable:
             obj.draw(screen)
         pygame.display.flip()
